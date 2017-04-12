967a464f3abd15ce42e4a41fd12cece47ce73f32
Migue-PC\MigueGrape <miguenoisegrape@gmail.com>
2017-04-11 23:23:21 -0500

    Ejemplo Semana 1 (Configurando Retrofit)

--- AplicacionContactos/app/src/main/java/com/anncode/aplicacioncontactos/restApi/ConstantesRestApi.java
+++ AplicacionContactos/app/src/main/java/com/anncode/aplicacioncontactos/restApi/ConstantesRestApi.java
@@ -6,6 +6,16 @@ package com.anncode.aplicacioncontactos.restApi;
 
 public final class ConstantesRestApi {
 
+    //https://api.instagram.com/v1/
+    public final String VERSION ="/v1/";
+    public final String ROOT_URL ="https://api.instagram.com"+ VERSION;
+    public final String ACCESS_TOKEN ="4946035890.f5487c5.820cc4d3a47f42ae82dbb8914a9cd6c0";
+    public final String KEY_ACCESS_TOKEN ="?access_token=";
+    public final String KEY_GET_INFORMATION_USER ="users/self/media/recent/";
+    public final String URL_GET_INFORMATION_USER = KEY_GET_INFORMATION_USER + KEY_ACCESS_TOKEN + ACCESS_TOKEN;
+
+    //https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN
+
 
 
 
---
+++ AplicacionContactos/app/src/main/java/com/anncode/aplicacioncontactos/restApi/EndpointsApi.java
@@ -0,0 +1,8 @@
+package com.anncode.aplicacioncontactos.restApi;
+
+/**
+ * Created by MigueGrape on 11/04/2017.
+ */
+
+public interface EndpointsApi {
+}
